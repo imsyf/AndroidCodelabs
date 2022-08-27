b61bf20fcd46bb29a6720866865f731eba3c1b8b
imsyf <alghozalyi@gmail.com>
2022-08-27 14:57:47 +0700

    `PagingAdvanced`: Displaying the initial list loading state

--- PagingAdvanced/app/src/main/java/im/syf/pagingadvanced/ui/SearchRepoActivity.kt
+++ PagingAdvanced/app/src/main/java/im/syf/pagingadvanced/ui/SearchRepoActivity.kt
@@ -3,8 +3,10 @@ package im.syf.pagingadvanced.ui
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.inputmethod.EditorInfo
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.lifecycleScope
@@ -135,6 +137,8 @@ class SearchRepoActivity : AppCompatActivity() {
         pagingData: Flow<PagingData<Repo>>,
         onScrollChanged: (UiAction.Scroll) -> Unit,
     ) {
+        retryButton.setOnClickListener { adapter.retry() }
+
         list.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 if (dy != 0) onScrollChanged(UiAction.Scroll(uiState.value.query))
@@ -166,6 +170,35 @@ class SearchRepoActivity : AppCompatActivity() {
                 shouldScrollToTop.collect { shouldScroll ->
                     if (shouldScroll) list.scrollToPosition(0)
                 }
+            }
+        }
+
+        lifecycleScope.launch {
+            adapter.loadStateFlow.collect { loadState ->
+                val isListEmpty =
+                    loadState.refresh is LoadState.NotLoading && adapter.itemCount == 0
+
+                empty.isVisible = isListEmpty
+                list.isVisible = !isListEmpty
+
+                // Show loading spinner during initial load or refresh.
+                progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+                // Show the retry state if initial load or refresh fails.
+                retryButton.isVisible = loadState.source.refresh is LoadState.Error
+
+                // Toast on any error, regardless of whether it came from RemoteMediator or PagingSource
+                val errorState = loadState.source.append as? LoadState.Error
+                    ?: loadState.source.prepend as? LoadState.Error
+                    ?: loadState.append as? LoadState.Error
+                    ?: loadState.prepend as? LoadState.Error
+                errorState?.let {
+                    Toast.makeText(
+                        this@SearchRepoActivity,
+                        "\uD83D\uDE28 Wooops ${it.error}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
             }
         }
     }
